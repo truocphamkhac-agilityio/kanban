6778a195fc17a3dd0008af2f35e30486eaffb570
Truoc Pham <truoc.phamkhac@asnet.com.vn>
2016-03-21 23:06:12 +0700

    Enable sourcemap and install npm-install-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const webpack = require('webpack');
+const NpmInstallPlugin = require('npm-install-webpack-plugin');
 
 const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
@@ -47,8 +48,12 @@ if (TARGET === 'start' || !TARGET) {
       port: process.env.PORT || 3000
     },
     plugins: [
-      new webpack.HotModuleReplacementPlugin()
-    ]
+      new webpack.HotModuleReplacementPlugin(),
+      new NpmInstallPlugin({
+        save: true // --save
+      })
+    ],
+    devtool: 'eval-source-map'
   });
 }
 
