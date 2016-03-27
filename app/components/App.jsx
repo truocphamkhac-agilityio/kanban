c528948799b1ec66039de4f2d69b2603bf2d79da
Truoc Pham <truoc.phamkhac@asnet.com.vn>
2016-03-27 15:11:58 +0700

    Add list notes using uuid to generate id of each note

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,10 +1,31 @@
 'use strict';
 
+import uuid from 'node-uuid';
 import React from 'react';
-import Note from './Note.jsx';
 
 export default class App extends React.Component {
   render() {
-    return <Note />;
+    const notes = [
+      {
+        id: uuid.v4(),
+        task: 'Learn Webpack'
+      },
+      {
+        id: uuid.v4(),
+        task: 'Learn Reacr'
+      },
+      {
+        id: uuid.v4(),
+        task: 'Do laundry'
+      }
+    ];
+
+    return (
+      <div>
+        <ul>
+          {notes.map(note => <li key={note.id}>{note.task}</li>)}
+        </ul>
+      </div>
+    );
   }
 }
