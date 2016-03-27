d9c70d57e465f06cea9bb1574240f2f48afb943d
Truoc Pham <truoc.phamkhac@asnet.com.vn>
2016-03-27 15:22:04 +0700

    Using state to render list of notes and add a new note.

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -4,28 +4,46 @@ import uuid from 'node-uuid';
 import React from 'react';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn Webpack'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Learn Reacr'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Do laundry'
+        }
+      ]
+    };
+  }
+
   render() {
-    const notes = [
-      {
-        id: uuid.v4(),
-        task: 'Learn Webpack'
-      },
-      {
-        id: uuid.v4(),
-        task: 'Learn Reacr'
-      },
-      {
-        id: uuid.v4(),
-        task: 'Do laundry'
-      }
-    ];
+    const notes = this.state.notes;
 
     return (
       <div>
+        <button onClick={this.addNote}>+</button>
         <ul>
           {notes.map(note => <li key={note.id}>{note.task}</li>)}
         </ul>
       </div>
     );
+  }
+
+  addNote = () => {
+    this.setState({
+      notes: this.state.notes.concat([{
+        id: uuid.v4(),
+        task: 'New task'
+      }])
+    });
   }
 }
