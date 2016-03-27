339b78983fb122165d68c3872b4da81d1f67884a
Truoc Pham <truoc.phamkhac@asnet.com.vn>
2016-03-27 14:27:40 +0700

    Update webpack config to display Note component

--- webpack.config.js
+++ webpack.config.js
@@ -9,21 +9,40 @@ const PATHS = {
   build: path.join(__dirname, 'build')
 };
 
-const common = {
-  entry: {
-    app: PATHS.app
-  },
+process.env.BABEL_ENV = TARGET;
 
+const common = {
+  // Entry accepts a path or an object of entries.
+  // The build chapter contains an example of the latter.
+  entry: PATHS.app,
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
   output: {
     path: PATHS.build,
     filename: 'bundle.js'
   },
-
   module: {
     loaders: [
       {
+        // Test expects a RegExp! Note the slashes!
         test: /\.css$/,
         loaders: ['style', 'css'],
+        // Include accepts either a path or an array of paths.
+        include: PATHS.app
+      },
+      {
+        test: /\.jsx?$/,
+        // Enable caching for improved performance during development
+        // It uses default OS directory by default. If you need something
+        // more custom, pass a path to it. I.e., babel?cacheDirectory=<path>
+        loader: 'babel',
+        query: {
+          cacheDirectory: true,
+          presets: ['react', 'es2015', 'survivejs-kanban']
+        },
+        // Parse only app files! Without this it will go through entire project.
+        // In addition to being slow, that will most likely result in an error.
         include: PATHS.app
       }
     ]
@@ -36,14 +55,19 @@ if (TARGET === 'start' || !TARGET) {
     devServer: {
       contentBase: PATHS.build,
 
+      // Enable history API fallback so HTML5 History API based routing works.
+      // This is a good default that will come in handy
+      // in more complicated setups
       historyApiFallback: true,
       hot: true,
       inline: true,
       progress: true,
 
+      // Display only errors to reduce the amount of output.
       stats: 'errors-only',
 
-      // if broadcast using '0.0.0.0' for host
+      // Parse host and port from env so this is easy to customize.
+      // If broadcast using '0.0.0.0' for host
       host: process.env.HOST,
       port: process.env.PORT || 3000
     },
