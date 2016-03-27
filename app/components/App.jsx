6be203328f5bdbe498ed1cce645c4add57ab4c67
Truoc Pham <truoc.phamkhac@asnet.com.vn>
2016-03-27 15:27:38 +0700

    Extracting notes to multi components

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,6 +2,7 @@
 
 import uuid from 'node-uuid';
 import React from 'react';
+import Notes from './Notes.jsx';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -31,9 +32,7 @@ export default class App extends React.Component {
     return (
       <div>
         <button onClick={this.addNote}>+</button>
-        <ul>
-          {notes.map(note => <li key={note.id}>{note.task}</li>)}
-        </ul>
+        <Notes notes={notes} />
       </div>
     );
   }
--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,3 +1,3 @@
 import React from 'react';
 
-export default () => <div>Learn React</div>;
+export default ({task}) => <div>{task}</div>;
